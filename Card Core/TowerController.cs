1c4e23fed6d7ba13bb76432e4cd0947964c551e8
donovan <dmontoya@unity.edu>
2026-03-10 15:42:04 -0400

    encapsulated upgrades array, added GetCurrentUpgrades and AddUpgrade methods.

--- Card Core/TowerController.cs
+++ Card Core/TowerController.cs
@@ -1,14 +1,30 @@
+using System.Linq;
 using UnityEngine;
 
 namespace _project.Scripts.Card_Core
 {
     public class TowerController : MonoBehaviour
     {
-        public ICard[] upgrades = new ICard[6];
-
+        private readonly ICard[] _upgrades = new ICard[6];
+        
+        public ICard[] GetCurrentUpgrades() => _upgrades;
+        
+        public ICard[] AddUpgrade(ICard upgrade)
+        {
+            if (_upgrades == null || _upgrades.Contains(upgrade) || _upgrades.Length >= 6)
+                return _upgrades;
+            for (var i = 0; i < _upgrades.Length; i++)
+            {
+                if (_upgrades[i] != null) continue;
+                _upgrades[i] = upgrade;
+                return _upgrades;
+            }
+            return _upgrades;
+        }
+        
         public bool ValidateUpgrades()
         {
-            return upgrades.Length <= 5;
+            return _upgrades.Length <= 5;
         }
     }
 }
