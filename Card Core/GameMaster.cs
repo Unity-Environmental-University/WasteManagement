37c0118217aa141083597168bcaac33f89c35a2d
donovan <dmontoya@unity.edu>
2026-03-10 15:48:55 -0400

    Removed unused fields and debug logs in CardController; added TowerManager and InterfaceManager setup in GameMaster.

--- Card Core/GameMaster.cs
+++ Card Core/GameMaster.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using _project.Scripts.UI;
 using UnityEngine;
 
 namespace _project.Scripts.Card_Core
@@ -7,16 +8,14 @@ namespace _project.Scripts.Card_Core
     ///     Central coordinator for the card game system. Manages integration between
     ///     deck, score, turn, and other core systems. Singleton.
     /// </summary>
-    //[RequireComponent(typeof(DeckManager))]
-    //[RequireComponent(typeof(ScoreManager))]
     [RequireComponent(typeof(TurnController))]
     public class GameMaster : MonoBehaviour
     {
         [Header("Major Game Components")]
-        // public DeckManager deckManager;
-        // public ScoreManager scoreManager;
         public TurnController turnController;
-
+        public TowerManager towerManager;
+        public InterfaceManager interfaceManager;
+        
         [Header("Debug")] public bool debugging;
 
         public static GameMaster Instance { get; private set; }
@@ -31,13 +30,11 @@ namespace _project.Scripts.Card_Core
 
             Instance = this;
 
-            // if (!scoreManager) scoreManager = GetComponent<ScoreManager>();
-            // if (!deckManager) deckManager = GetComponent<DeckManager>();
             if (!turnController) turnController = GetComponent<TurnController>();
+            if (!towerManager) towerManager = GetComponent<TowerManager>();
+            if (!interfaceManager) interfaceManager = GetComponentInChildren<InterfaceManager>();
 
             var missing = new List<string>();
-            // if (!scoreManager) missing.Add(nameof(scoreManager));
-            // if (!deckManager) missing.Add(nameof(deckManager));
             if (!turnController) missing.Add(nameof(turnController));
             if (missing.Count > 0)
                 Debug.LogWarning($"[CardGameMaster] Missing components: {string.Join(", ", missing)}");
--- UI/CardController.cs
+++ UI/CardController.cs
@@ -1,35 +1,29 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace _project.Scripts.UI
 {
     public class CardController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        public Image cardImage;
-        
         private RectTransform _rectTransform;
         private Vector2 _initAnchoredPosition;
         private const float JumpHeightFraction = 0.8f;
 
         private void Start()
         {
-            cardImage = GetComponent<Image>();
             _rectTransform = GetComponent<RectTransform>();
             _initAnchoredPosition = _rectTransform.anchoredPosition;
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            Debug.Log("Mouse Over");
             var offset = _rectTransform.rect.height * JumpHeightFraction;
             _rectTransform.DOLocalMoveY(_initAnchoredPosition.y + offset, 0.1f);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            Debug.Log("Mouse Exit");
             _rectTransform.DOLocalMoveY(_initAnchoredPosition.y, 0.1f);
         }
     }
